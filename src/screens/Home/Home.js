c81aa5ae1acfba6d0060f3027b977367bd47865b
Damian <damianmdst@gmail.com>
2019-01-12 20:04:50 -0300

    Create styled components to wrapp video details

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -6,15 +6,36 @@ import 'video-react/dist/video-react.css';
 import { getVideos } from '../../services';
 
 const Container = styled.section`
-  border: 1px solid red;
+  background-color: #f9f9f9;
+  padding: 0.5rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  width: 700px;
+  margin: 0 auto;
+`;
+
+const VideoTitle = styled.span`
+  color: #2a3438;
+  font-weight: bold;
+  font-size: 1.3rem;
+`;
+
+const VideoDescription = styled.p`
+  color: #84898b;
+  font-size: 0.8rem;
 `;
 
 const VideoContainer = styled.section`
-  width: 600px;
+  width: 700px;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0.2rem;
 `;
 
 export default class Home extends React.Component {
@@ -34,16 +55,16 @@ export default class Home extends React.Component {
           <VideoContainer>
             <Player playsInline poster={video.poster} src={video.video} />
           </VideoContainer>
-          <div>
+          <Footer>
             <div>
-              <h1>{video.title}</h1>
-              <p>{video.description}</p>
+              <VideoTitle>{video.title}</VideoTitle>
+              <VideoDescription>{video.description}</VideoDescription>
             </div>
             <div>
               <h4>Prev Video</h4>
               <h4>Next Video</h4>
             </div>
-          </div>
+          </Footer>
         </Container>
       );
     }
