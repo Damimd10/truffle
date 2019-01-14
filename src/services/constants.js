f988cf9b7d1da07d1e210ae403c99ebf92ee2112
Damian <damianmdst@gmail.com>
2019-01-13 21:31:48 -0300

    Add secure domain to fetch the endpoint

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,2 +1,2 @@
 export const URL =
-  'http://tm-kitchen-api-alpha.herokuapp.com/videos?api_key=homework&auth_token=1&limit=10&workflow_status=ready';
+  'https://tm-kitchen-api-alpha.herokuapp.com/videos?api_key=homework&auth_token=1&limit=10&workflow_status=ready';
